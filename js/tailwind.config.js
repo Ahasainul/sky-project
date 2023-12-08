db341a5a5705c6cf16b19b757bbe4183d8825f9a
Ahasainul <adnan31615asif@gmail.com>
2023-12-08 13:15:58 +0600

    first commit

---
+++ js/tailwind.config.js
@@ -0,0 +1,33 @@
+tailwind.config = {
+    theme: {
+      extend: {
+    //  clolour 
+    colors: {
+        text: '#da373d',
+        bg1: '#6A55EA',
+        bg2: '#da373d',
+        bg3: '#da373d',
+        bg4: '#da373d',
+        bg5: '#da373d',
+      },
+    //  clolour 
+    // width 
+    maxWidth: {
+        'container': '1170px',
+      },
+    // width
+    // background img  
+    backgroundImage: {
+        'banner': "url('./images/bg.png')",
+      
+      },
+    // background img  
+    // font family  
+    fontFamily: {
+      'sans': ['Open Sans', 'sans-serif',],
+      
+    },
+    // font family  
+      }
+    }
+  }
